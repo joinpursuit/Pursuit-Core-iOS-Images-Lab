f3f5947258cb4f9d0a5fecb4cdcaff30bd60db9d
Sam <samroman3@gmail.com>
2019-09-05 15:17:31 -0400

    Added outlets for Storyboard elements: UIImageView, Stepper, Buttons, and Textfield

---
+++ ImagesLab/ImagesLab/Managers/AppError.swift
@@ -0,0 +1,23 @@
+//
+//  AppError.swift
+//  breweryProject
+//
+//  Created by Sam Roman on 9/5/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case badJSONError
+    case networkError
+    case noDataError
+    case badHTTPResponse
+    case badUrl
+    case notFound //404 status code
+    case unauthorized //403 and 401 status code
+    case badImageData
+    case other(errorDescription: String)
+}
+
+
--- ImagesLab/ImagesLab/ViewController.swift
+++ ImagesLab/ImagesLab/ViewController.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var comicView: UIImageView!
+    
+    
+    @IBOutlet weak var stepperOut: UIStepper!
+    
+    
+    @IBOutlet weak var textFieldOut: UITextField!
+    
+    
+    @IBAction func mostRecentButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func randomButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
