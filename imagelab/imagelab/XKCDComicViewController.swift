52defe4592b65a3d1f71a140ff2a693374c18a81
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-12-10 10:42:21 -0500

    created UI elements for xkcd comic viewer.

---
+++ imagelab/imagelab/XKCDComicViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  imagelab
+//
+//  Created by Ahad Islam on 12/10/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class XKCDComicViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func mostRecentButtonPressed(_ sender: Any) {
+    }
+    
+}
+
